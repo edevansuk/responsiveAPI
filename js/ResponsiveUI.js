5171f3da3f292e11aed160f363d8e22bb95fce96
edevansuk <edward@applemarketing.co.uk>
2014-02-09 21:15:38 +0000

    Fixed Firefox bug so the test if the BP has changed works in FF

--- js/ResponsiveUI.js
+++ js/ResponsiveUI.js
@@ -5,9 +5,9 @@
 	var ResponsiveUI = function(options){
 		this.settings = {
 			supported: true, // Checked and validated in this.init();
-			lastBreakPoint: '' // Can use this to check if breakpoint has changed
+			lastBreakPoint: '' // Stores the last breakpoint to test if it's changed
 		};
-		
+
 		// extends the settings object, so you can set default breakPoint setting
 		if (options) {
 			for(var i in options)
@@ -16,7 +16,7 @@
 		
 		this.init(); 
 	};
-	
+
 	/*
 	 * Delays rechecking a function based on an event such as window
 	 * resizing. Good for checking window width for example while reducing
@@ -36,31 +36,30 @@
 			if (callNow) func.apply(context, args);
 		};
 	};
-	
+
 	/*
 	 * Check the responsive state
 	 */
 	ResponsiveUI.prototype.responsiveCheck = function(){
 		var thisState = window.getComputedStyle(document.querySelector('body'), ':before').getPropertyValue('content');
-		var thisStateString = thisState.toString();
-		this.settings.breakPoint = thisStateString;
+		this.settings.breakPoint = thisState.toString();	
 	};
-	
+
 	/*
 	 * Image handling
 	 */
-	ResponsiveUI.prototype.resizeImages = function(){
+	ResponsiveUI.prototype.images = function(){
 		// Find all elements on the page with data-[namespace]-src
 		var pageImg = document.querySelectorAll('[data-responsive-src]'),
 			pageImgLength = pageImg.length;
-				
+
 		for (var i=0;i<pageImgLength;i++) { 
 			var thisImgObj = pageImg[i],
 				thisImgObjAtt = thisImgObj.getAttribute('data-responsive-src'),
 				imgObjAttSplit = thisImgObjAtt.split(','),
 				imgObjAttSplitLgth = imgObjAttSplit.length;
-					
-					
+
+
 			for(var s=0;s<imgObjAttSplitLgth;s++){
 				// Create local versions of objects for performance
 				var splitItem = imgObjAttSplit[s], 
@@ -68,10 +67,10 @@
 					sizeSplitStringFF = '\"' + sizeSplit[0].toString() + '\"',
 					sizeSplitString = sizeSplit[0].toString(),
 					cachedBreakpoint =  this.settings.breakPoint;
-					
+
 				if(sizeSplitString == cachedBreakpoint || sizeSplitStringFF == cachedBreakpoint) {
-	                   thisImgObj.src = sizeSplit[1];
-				}
+                    thisImgObj.src = sizeSplit[1];
+                }
 			}
 		}
 	};
@@ -81,8 +80,7 @@
 	 */
 	ResponsiveUI.prototype.breakPointChanged = function(){
 		console.log('bpChecker ran');
-		var $this = this;
-			localBP = this.settings.breakPoint,
+		var localBP = this.settings.breakPoint,
 			localLBP = this.settings.lastBreakPoint;
 		
 		// Only update images if a new breakpoint is activated
@@ -99,7 +97,6 @@
 	 */
 	ResponsiveUI.prototype.init = function(){
 		var $this = this;
-
 		// Checks for method support and sets a value that can be interrogated later
 		if(!window.querySelector && !window.addEventListener) {
 			$this.settings.supported = false;
@@ -112,7 +109,7 @@
 			$this.responsiveCheck();
 		}, 100), false);
 	};
-	
+
 	exports.ResponsiveUI = ResponsiveUI;
 }(window));
 

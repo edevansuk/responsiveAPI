49e56b64e6a48db030291fe32d2b4d702f804fb1
edevansuk <edward@applemarketing.co.uk>
2014-02-09 20:23:10 +0000

    Put page JS in a seperate file and added functionality to check if the breakpoint state has changed. Helpful when you only want to fire actions if there's been a change (such as updating an image)

--- js/ResponsiveUI.js
+++ js/ResponsiveUI.js
@@ -4,7 +4,8 @@
 	 */
 	var ResponsiveUI = function(options){
 		this.settings = {
-			supported: true // Checked and validated in this.init();
+			supported: true, // Checked and validated in this.init();
+			lastBreakPoint: '' // Can use this to check if breakpoint has changed
 		};
 		
 		// extends the settings object, so you can set default breakPoint setting
@@ -41,24 +42,25 @@
 	 */
 	ResponsiveUI.prototype.responsiveCheck = function(){
 		var thisState = window.getComputedStyle(document.querySelector('body'), ':before').getPropertyValue('content');
-		this.settings.breakPoint = thisState.toString();	
+		var thisStateString = thisState.toString();
+		this.settings.breakPoint = thisStateString;
 	};
 	
 	/*
 	 * Image handling
 	 */
-	ResponsiveUI.prototype.images = function(){
+	ResponsiveUI.prototype.resizeImages = function(){
 		// Find all elements on the page with data-[namespace]-src
 		var pageImg = document.querySelectorAll('[data-responsive-src]'),
 			pageImgLength = pageImg.length;
-			
+				
 		for (var i=0;i<pageImgLength;i++) { 
 			var thisImgObj = pageImg[i],
 				thisImgObjAtt = thisImgObj.getAttribute('data-responsive-src'),
 				imgObjAttSplit = thisImgObjAtt.split(','),
 				imgObjAttSplitLgth = imgObjAttSplit.length;
-				
-				
+					
+					
 			for(var s=0;s<imgObjAttSplitLgth;s++){
 				// Create local versions of objects for performance
 				var splitItem = imgObjAttSplit[s], 
@@ -66,11 +68,29 @@
 					sizeSplitStringFF = '\"' + sizeSplit[0].toString() + '\"',
 					sizeSplitString = sizeSplit[0].toString(),
 					cachedBreakpoint =  this.settings.breakPoint;
-				
+					
 				if(sizeSplitString == cachedBreakpoint || sizeSplitStringFF == cachedBreakpoint) {
-                    thisImgObj.src = sizeSplit[1];
-                }
+	                   thisImgObj.src = sizeSplit[1];
+				}
 			}
+		}
+	};
+	
+	/*
+	 * Check the breakpoint has changed or not
+	 */
+	ResponsiveUI.prototype.breakPointChanged = function(){
+		console.log('bpChecker ran');
+		var $this = this;
+			localBP = this.settings.breakPoint,
+			localLBP = this.settings.lastBreakPoint;
+		
+		// Only update images if a new breakpoint is activated
+		if(localBP !== localLBP){
+			this.settings.lastBreakPoint = localBP;
+			return true;
+		} else {
+			return false;
 		}
 	};
 	
@@ -79,6 +99,7 @@
 	 */
 	ResponsiveUI.prototype.init = function(){
 		var $this = this;
+
 		// Checks for method support and sets a value that can be interrogated later
 		if(!window.querySelector && !window.addEventListener) {
 			$this.settings.supported = false;
